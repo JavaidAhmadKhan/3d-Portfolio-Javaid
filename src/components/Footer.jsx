429a5197e7977fbba89f4ca8f7ca74632955176d
JavaidAhmadKhan <relaxjavaid@gmail.com>
2023-03-04 11:40:32 +0530

    completed ui

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { FaFacebookSquare } from 'react-icons/fa';
+import { AiFillInstagram, AiFillGithub, AiFillLinkedin, AiFillTwitterCircle } from 'react-icons/ai';
+
+const Footer = () => {
+    return (
+        <>
+            <div className='flex items-center px-8 md:px-18 lg:px-24 py-3'>
+                <h2 className='text-[16px]'>Javaid@2023</h2>
+                <div className='flex gap-2 ml-auto '>
+                    <a className='text-blue-500 hover:text-green-500 text-[28px]' href="https://www.instagram.com/javaidkhaan/" target="_blank" rel="noopener noreferrer">
+                        <AiFillInstagram />
+                    </a>
+                    <a className='text-blue-500 hover:text-green-500 text-[28px]' href="https://www.facebook.com/javaid.khan19/" target="_blank" rel="noopener noreferrer">
+                        <FaFacebookSquare />
+                    </a>
+                    <a className='text-blue-500 hover:text-green-500 text-[28px]' href="https://github.com/JavaidAhmadKhan" target="_blank" rel="noopener noreferrer">
+                        <AiFillGithub />
+                    </a>
+                    <a className='text-blue-500 hover:text-green-500 text-[28px]' href="https://www.linkedin.com/in/javaid-khan-183a41136/" target="_blank" rel="noopener noreferrer">
+                        <AiFillLinkedin />
+                    </a>
+                    <a className='text-blue-500 hover:text-green-500 text-[28px]' href="https://twitter.com/Javaid_Khan19" target="_blank" rel="noopener noreferrer">
+                        <AiFillTwitterCircle />
+                    </a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section className={`relative w-full h-screen mx-auto `}>
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
@@ -19,12 +19,11 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Javaid</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I develop 3D visuals, Mobile Apps, Web Apps <br className='sm:block hidden' />
           </p>
         </div>
       </div>
-    
+
       <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
